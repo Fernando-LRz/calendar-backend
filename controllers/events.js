b26636bf5436cceafab32bccf9e57d58c8117d88
Fernando LR <fernandolr.0915@gmail.com>
2024-05-23 22:48:25 -0600

    create event

--- controllers/events.js
+++ controllers/events.js
@@ -1,5 +1,7 @@
 const { response } = require('express');
 
+const Event = require('../models/Event');
+
 const getEvents = async(req, res = response) => {
     try {        
 
@@ -20,10 +22,14 @@ const getEvents = async(req, res = response) => {
 
 const createEvent = async(req, res = response) => {
     try {        
+        const event = new Event(req.body);
+
+        event.user = req.uid;
+        await event.save();
 
         res.json({
             ok: true,
-            msg: 'createEvent'
+            event: event
         });
 
     } catch (error) {
--- routes/events.js
+++ routes/events.js
@@ -1,14 +1,29 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
 
 const { getEvents, createEvent, updateEvent, deleteEvent } = require('../controllers/events');
+const { validateFields } = require('../middlewares/validateFields');
 const { validateJWT } = require('../middlewares/validateJWT');
+const { isDate } = require('../helpers/isDate');
 
 const router = Router();
 router.use(validateJWT);
 
 router.get('/', getEvents);
-router.post('/', createEvent);
+
+router.post(
+    '/', 
+    [
+        check('title', 'The title is required').not().isEmpty(),
+        check('start', 'The start date is required').custom(isDate),
+        check('end', 'The end date is required').custom(isDate),
+        validateFields
+    ], 
+    createEvent
+);
+
 router.put('/:id', updateEvent);
+
 router.delete('/:id', deleteEvent);
 
 module.exports = router;
